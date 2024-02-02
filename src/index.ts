249a69765d99291e0fa81234e37215b190a7f93e
David Forbes <davidforbesi@hotmail.com>
2024-02-02 09:15:18 +0000

    install and setup pug

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+// mongoose connection
+
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("My Server");
+  res.render("index");
 });
 
 app.listen(port, () => {
