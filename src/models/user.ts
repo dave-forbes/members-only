b1c8926783e07990b52259e61f1fa8df9c5c86fc
David Forbes <davidforbesi@hotmail.com>
2024-02-02 12:50:41 +0000

    formatted with prettier and adjusted fullName

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,14 +3,21 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  firstName: { type: String, required:true, maxLength: 100 },
-  lastName: { type: String, required:true, maxLength: 100 },
-  email: { type: String, required: true, maxLength: 100, unique: true, index: true, lowercase: true },
-  password: { type: String, required:true, maxLength: 100 },
-  membership: {type: String, enum: ["member", "admin", "non-member"]},
+  firstName: { type: String, required: true, maxLength: 100 },
+  lastName: { type: String, required: true, maxLength: 100 },
+  email: {
+    type: String,
+    required: true,
+    maxLength: 100,
+    unique: true,
+    index: true,
+    lowercase: true,
+  },
+  password: { type: String, required: true, maxLength: 100 },
+  membership: { type: String, enum: ["member", "admin", "non-member"] },
 });
 
-UserSchema.virtual("full-name").get(function() {
+UserSchema.virtual("fullName").get(function () {
   return `${this.firstName} ${this.lastName}`;
 });
 
