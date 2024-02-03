a0276d444e12bfc0f157cf1d0a3d20d9b7618e18
David Forbes <davidforbesi@hotmail.com>
2024-02-03 11:32:38 +0000

    separate passport auth logic to separate file

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,7 @@ import createHttpError from "http-errors";
 import session from "express-session";
 import logger from "morgan";
 import router from "./router";
-import passport from "passport";
-import { Strategy as LocalStrategy } from "passport-local";
-import User from "./models/user";
-import bcrypt from "bcrypt";
+import passport from "./config/passportConfig";
 import flash from "express-flash";
 
 dotenv.config();
@@ -46,49 +43,6 @@ app.set("view engine", "pug");
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 
 app.use(flash());
-
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({ email: username });
-
-      if (!user) {
-        console.log("User not found:", username);
-        return done(null, false, { message: "Incorrect username" });
-      }
-
-      const match = await bcrypt.compare(password, user.password);
-
-      if (!match) {
-        console.log("Incorrect password for user:", username);
-        return done(null, false, { message: "Incorrect password" });
-      }
-
-      console.log("Authentication successful for user:", username);
-      return done(null, user);
-    } catch (err) {
-      console.error("Error during authentication:", err);
-      return done(err);
-    }
-  })
-);
-
-passport.serializeUser((user: any, done) => {
-  console.log("Serializing user:", user);
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id: string, done) => {
-  console.log("Deserializing user with id:", id);
-  try {
-    const user = await User.findById(id);
-    console.log("Deserialized user:", user);
-    done(null, user);
-  } catch (err) {
-    console.error("Error during deserialization:", err);
-    done(err);
-  }
-});
 
 app.use(passport.initialize());
 app.use(passport.session());
